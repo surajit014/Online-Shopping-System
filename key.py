56acb0c7c0b9eea6f396c47dc53f0a218328e842
surajit014 <surajitmishra0987@gmail.com>
2025-04-23 16:29:47 +0530

    Update key.py

--- key.py
+++ key.py
@@ -2,4 +2,4 @@ import os
 
 # Generate 24 random bytes (192 bits) and encode as hex
 secret_key = os.urandom(24).hex()
-print(secret_key)  # Example: '76eff09d4c770294ea1cb26bfed1f05ab3fabfbceb23ef67'
+print(secret_key)  
